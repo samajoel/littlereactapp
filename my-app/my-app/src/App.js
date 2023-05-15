d8f93b884ccdcdd9c92df66dff516cbaa7c193a7
joel-samaniego <joel.samaniego@university.mentormate.com>
2023-05-15 18:57:54 -0400

    Adding styles, adding data

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,11 +1,51 @@
 import React from "react";
+import { products } from "./data";
+import "./App.css";
 
-export default function App() {
+function Searchbar() {
   return (
-    <>
-      <h1>
-        Welcome to React App thats build using Webpack and Babel separately
-      </h1>
-    </>
+    <div className="search">
+      <h3>Search</h3>
+      <input></input>
+    </div>
+  );
+}
+
+function CheckBox() {
+  return (
+    <div className="input">
+      <input type="checkbox"></input>
+      <h5>Only Show Products in Stock</h5>
+    </div>
+  );
+}
+
+function Table() {
+  return (
+    <div className="tablecontainer">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+        {products.map((product) => (
+          <tr>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default function Main() {
+  return (
+    <div className="main">
+      <h1> Iron Store </h1>
+      <Searchbar />
+      <CheckBox />
+      <Table />
+    </div>
   );
 }
