94870440b675460ba50f2f55f28e048ce203aaa4
joel-samaniego <joel.samaniego@university.mentormate.com>
2023-05-15 18:40:42 -0400

    Correcting paths, adding git ignore, correcting webpack config

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function App() {
   return (
-    <h1>Welcome to React App thats build using Webpack and Babel separately</h1>
+    <>
+      <h1>
+        Welcome to React App thats build using Webpack and Babel separately
+      </h1>
+    </>
   );
 }
